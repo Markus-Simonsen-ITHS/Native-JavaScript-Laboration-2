dfb5294b5e463495b70cd7a2f8f0215325fb4273
Markus Simonsen <33492046+Markussim@users.noreply.github.com>
2021-10-27 13:59:11 +0200

    Fixed github auth

--- api/index.js
+++ api/index.js
@@ -3,12 +3,34 @@ const app = express()
 const port = 3000
 const cors = require('cors')
 const ip = require('ip')
+const { json } = require('express')
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+require("dotenv").config();
 
 app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Welcome!')
 });
+
+app.get('/fakeCallback', async (req, res) => {
+    const request = await fetch('https://github.com/login/oauth/access_token', {
+        body: `{ "client_id": "${process.env.CLIENT_ID}", "client_secret": "${process.env.SECRET}", "code": "${req.query.code}" }`,
+        headers: {
+            'Content-Type': 'application/json',
+            'accept': "application/json"
+        },
+        method: 'POST'
+    })
+
+    // console.log(await request.json())
+
+    const authResponse = await request.text();
+
+    console.log(authResponse)
+
+    res.send(JSON.parse(authResponse)['access_token'])
+})
 
 app.listen(port, () => {
     console.log(`\nApp running at:\n- Local: \x1b[36mhttp://localhost:${port}/\x1b[0m\n- Network \x1b[36mhttp://${ip.address()}:${port}/\x1b[0m\n\nTo run for production, run \x1b[36mnpm run start\x1b[0m`)
