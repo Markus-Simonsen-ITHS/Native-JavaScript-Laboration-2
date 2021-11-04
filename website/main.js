c66080d281679c3924c68f2074187144562413a4
Markus Simonsen <33492046+Markussim@users.noreply.github.com>
2021-11-04 13:42:02 +0100

    Switched api

--- website/main.js
+++ website/main.js
@@ -1,20 +1,87 @@
-let githubParams = {}
+const url = "https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/1/station/71420/period/latest-day/data.json"
+const rainUrl = "https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/7/station/71420/period/latest-hour/data.json"
+let audio = new Audio('rain-01.mp3')
 
-githubParams.headers = {
-    "Authorization": `Bearer ${localStorage.getItem('code')}`
+// Get the data from the url
+function fetchData() {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            // Extract the last 20 temperature values
+            let temperatureValues = data.value.slice(-20)
+
+            // Print temperature values to console
+            console.log(temperatureValues)
+
+            chartRenderer(temperatureValues)
+
+        })
 }
 
-const userInfoPromise = fetch("https://api.github.com/user", githubParams)
+// Get rain data from url
+async function fetchRainData() {
+    let response = await fetch(rainUrl)
+    let data = await response.json()
+    let rainValues = data.value.slice(-20)
+    if (rainValues[rainValues.length - 1].value > 0) {
+        playPauseRain(true)
+    } else {
+        playPauseRain(false)
+    }
+}
 
-document.addEventListener("DOMContentLoaded", async function () {
-    let loggedInMessage = document.getElementById("loggedIn")
-    let profilePic = document.getElementById("pfp")
-    const response = await userInfoPromise
-    const userInfo = await response.json()
-    if (!userInfo.login) window.location = "/website/login"
-    console.log(userInfo)
-    profilePic.setAttribute("src", userInfo.avatar_url + "&s=50")
-    const name = userInfo.name ? userInfo.name : userInfo.login
-    loggedInMessage.innerText = "Logged in as " + name
+fetchRainData()
 
-});
+const average = (array) => {
+    let sum = 0
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i]
+    }
+    return sum / array.length
+}
+
+// Run the fetchData function every 60 seconds
+setInterval(fetchData, 60000)
+
+fetchData()
+
+function chartRenderer(smhiObject) {
+    // Create a new chart
+    const temps = smhiObject.map(obj => obj.value)
+    const dates = smhiObject.map(obj => obj.date)
+    const labels = dates.map(unixToISO)
+
+    const obj = {
+        chart: {
+            type: 'line'
+        },
+        series: [
+            {
+                name: 'Temperature',
+                data: temps
+            }
+        ],
+        xaxis: {
+            categories: labels
+        },
+        colors: ['#0066ff']
+    }
+
+    const chart = new ApexCharts(document.querySelector('#chart'), obj)
+
+    chart.render()
+}
+
+// Unix time to LocaleTimeString
+function unixToISO(unix) {
+    let date = new Date(unix)
+    return date.toLocaleTimeString()
+}
+
+function playPauseRain(play) {
+    if (play) {
+        audio.play()
+    } else {
+        audio.pause()
+    }
+}
