1bea01ba65df23539eea56019fef0b39def57f4e
Markus Simonsen <33492046+Markussim@users.noreply.github.com>
2021-11-09 18:09:28 +0100

    Added recents

--- website/main.js
+++ website/main.js
@@ -1,4 +1,6 @@
 const mainList = document.querySelector('#mainList');
+const recentDom = document.querySelector('#recent');
+const recentList = "recentlist";
 const search = document.querySelector('#city');
 
 search.focus();
@@ -67,10 +69,58 @@ function createList(cityList) {
     } else {
         cityList.forEach(city => {
             const li = document.createElement('li');
-            li.innerHTML = `<a href="weather/index.html?id=${city.id}&cityName=${city.cityName}">${city.cityName}</a>`;
+            li.innerHTML = `<a href="weather/index.html?id=${city.id}&cityName=${city.cityName}" onclick='addToRecent(event);'>${city.cityName}</a>`;
             mainList.appendChild(li);
         });
     }
+
+    // Check if recents are empty
+    if (localStorage.getItem(recentList) === null) {
+        recentDom.innerHTML = '';
+    } else {
+        const recentListObj = JSON.parse(localStorage.getItem(recentList));
+        recentDom.innerHTML = '';
+        let recentText = document.createElement('p');
+        recentText.innerHTML = 'Recent:';
+        recentDom.appendChild(recentText);
+        recentListObj.forEach(city => {
+            const li = document.createElement('li');
+            li.innerHTML = `<a href="weather/index.html?id=${city.id}&cityName=${city.cityName}" onclick='addToRecent(event);'>${city.cityName}</a>`;
+            recentDom.appendChild(li);
+        });
+    }
+}
+
+// Runs when user clicks link
+function addToRecent(ev) {
+    const cityName = ev.target.text;
+    const id = ev.target.href.split('=')[1];
+    let recentListObj = JSON.parse(localStorage.getItem(recentList));
+
+    if ( recentListObj === null ) {
+        recentListObj = [];
+    }
+
+    // Check if city is already in recents and add it to start of list
+    if (recentListObj !== null || recentListObj.length > 0) {
+        if (recentListObj.find(city => city.id === id)) {
+            recentListObj.splice(recentListObj.indexOf(recentListObj.find(city => city.id === id)), 1);
+        }
+    }
+
+    // Add city to start of list
+    recentListObj.unshift({
+        cityName: cityName,
+        id: id
+    });
+
+    // Limit list to 5 items
+    if (recentListObj.length > 3) {
+        recentListObj.pop();
+    }
+
+    // Save list to local storage
+    localStorage.setItem(recentList, JSON.stringify(recentListObj));
 }
 
 createList(cityList);
