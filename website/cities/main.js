44ade596698ff6ba4a0ac159b490bfe7156b7490
Markus Simonsen <33492046+Markussim@users.noreply.github.com>
2021-11-11 14:32:46 +0100

    SIte done

--- website/cities/main.js
+++ website/cities/main.js
@@ -8,6 +8,7 @@ window.onload = function () {
 
     // Run addCity on submit of addCityForm
     document.querySelector("#addCityForm").addEventListener("submit", addCity)
+
 }
 
 // Gets all cities from the API
@@ -24,10 +25,12 @@ function createCityDom(city) {
     const cityContainer = document.querySelector("#CitiesList")
     const cityElement = document.createElement("div")
     cityElement.classList.add("city")
+
     cityElement.innerHTML = `
         <h2>${city.name}</h2>
         <p>${Intl.NumberFormat().format(city.population)}</p>
-
+        <button class="delete" onclick="deleteCity('${city.id}', this)">Delete</button>
+        <button class="edit" onclick="editCity('${city.id}', this, '${city.name}', '${city.population}')">Edit</button>
     `
     cityContainer.appendChild(cityElement)
 }
@@ -47,24 +50,89 @@ async function addCity(e) {
     e.preventDefault()
     const cityName = document.querySelector("#cityName").value
     const cityPopulation = document.querySelector("#cityPopulation").value
-    const city = {
-        name: cityName,
-        population: parseInt(cityPopulation) 
+    const cityId = document.querySelector("#cityId").value
+
+    let data
+
+    if (cityId.length === 0) {
+        const city = {
+            name: cityName,
+            population: parseInt(cityPopulation)
+        }
+        const response = await fetch(apiUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(city)
+        })
+        data = await response.json()
+
+        document.querySelector("#cityName").value = ""
+        document.querySelector("#cityPopulation").value = ""
+        document.querySelector("#cityId").value = ""
+    } else {
+        const city = {
+            name: cityName,
+            population: parseInt(cityPopulation),
+            id: cityId
+        }
+        const response = await fetch(apiUrl + cityId, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify(city)
+        })
+
+        data = await response.text()
+
+        document.querySelector("#cityName").value = ""
+        document.querySelector("#cityPopulation").value = ""
+        document.querySelector("#cityId").value = ""
     }
-    const response = await fetch(apiUrl, {
-        method: "POST",
-        headers: {
 
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(city)
-    })
 
-    const data = await response.json()
-
-    if(data.error) {
+    if (data.error) {
         alert(data.error)
     } else {
         getCities()
     }
+}
+
+// Delete a city
+async function deleteCity(id, button) {
+    button.disabled = true
+
+    const response = await fetch(apiUrl + id, {
+        method: "DELETE"
+    })
+    const data = await response.text()
+    if (data.error) {
+        alert(data.error)
+    } else {
+        getCities()
+    }
+}
+
+// Edit a city
+async function editCity(id, button, name, population) {
+    document.querySelector("#cityName").value = name
+    document.querySelector("#cityPopulation").value = population
+    document.querySelector("#cityId").value = id
+
+    if (document.querySelector("#cityId").value.length === 0) {
+        document.querySelector("#submitButton").value = "Add city"
+    } else {
+        document.querySelector("#submitButton").value = "Update city"
+    }
+
+    document.querySelector("#addCityForm").style.animation = ""
+
+    setTimeout(() => {
+        document.querySelector("#addCityForm").style.animation = "bounce .5s"
+    }, 1)
+
+    
 }
